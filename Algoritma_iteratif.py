78a2621a73352c278a488d2c7d63a622cc65309c
Afif Rijal Azzami <afifrijal03@gmail.com>
2025-01-02 17:49:00 +0700

    Update Algoritma_iteratif.py

--- Algoritma_iteratif.py
+++ Algoritma_iteratif.py
@@ -1,16 +1,38 @@
-def cari_nim_iteratif(array, nim_cari):
-    for i in range(len(array)):
-        if array[i] == nim_cari: 
-            return i
-    return -1 
+import time
+
+def cari_nim_binary_iteratif(array, nim_cari):
+    low = 0
+    high = len(array) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] == nim_cari:
+            return mid
+        elif array[mid] < nim_cari:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
 
 array_nim = [f"23111022{i}" for i in range(1, 1001)]
 
 nim_cari = input("Masukkan NIM yang ingin dicari: ")
+start_time = time.time()
 
-hasil = cari_nim_iteratif(array_nim, nim_cari)
+# Melakukan pencarian
+hasil = cari_nim_binary_iteratif(array_nim, nim_cari)
 
+# Mencatat waktu setelah pencarian
+end_time = time.time()
+
+# Menghitung waktu eksekusi
+running_time = end_time - start_time
+
+# Menampilkan hasil pencarian dan waktu eksekusi
 if hasil != -1:
     print(f"NIM ({nim_cari}) tersebut adalah mahasiswa Tel-U yang terdapat pada indeks {hasil}.")
 else:
     print(f"NIM ({nim_cari}) tersebut bukan mahasiswa Tel-U.")
+
+print(f"Waktu yang dibutuhkan untuk pencarian: {running_time:.6f} detik.")
